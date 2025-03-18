19e894dfa44d0c1fd71caa2f982cf393becbeb3b
Android Studio <devandroid@ort>
2025-03-18 08:27:21 -0300

    css done

---
+++ app.js
@@ -0,0 +1,15 @@
+const notaMin=1, notaMax=10;
+let mate=document.getElementById('mate');
+let lengua=document.getElementById('lengua');
+let efsi=document.getElementById('efsi');
+let promBtn=document.getElementById('prom');
+let mayorNota=document.getElementById('mayorNota');
+let pVisor=document.getElementById('visor');
+
+const validarNota=(nota)=>{
+    return nota>=notaMin&&nota<=notaMax;
+}
+
+mate.addEventListener('input',()=>{
+    
+});
